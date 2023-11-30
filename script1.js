f628474185298d5f58fbba05977980b7b1a07474
TrigunTeja33 <146958590+TrigunTeja33@users.noreply.github.com>
2023-11-30 18:35:24 +0530

    script1.js

--- script1.js
+++ script1.js
@@ -11,6 +11,36 @@ const formPopup = document.querySelector(".form-popup");
 const hidePopupBtn = formPopup.querySelector(".close-btn");
 const signupLoginLink = formPopup.querySelectorAll(".bottom-link a");
 
+const userStatusElement = document.getElementById('user-status');
+const loginForm = document.getElementById('login-form');
+const logoutBtn = document.getElementById('logout-btn');
+const signupForm = document.getElementById('signup-form');
+
+// Check if the user is already logged in
+updateUserStatus();
+
+function updateUserStatus(username) {
+  if (isLoggedIn()) {
+    userStatusElement.innerHTML = `Logged In as <b>${username}</b> | <button id="logout-btn" style="width:140px;height:40px;border:none;outline:none;background:#fff;color:#275360;font-size:1rem;font-weight:600;padding:10px18px;border-radius:3px;cursor:pointer;transition:ease;">Logout</button>`;
+    document.getElementById('logout-btn').addEventListener('click', logout);
+  } else {
+    userStatusElement.innerHTML = 'Not Logged In | <button id="login-btn" style="width:140px;height:40px;border:none;outline:none;background:#fff;color:#275360;font-size:1rem;font-weight:600;padding:10px18px;border-radius:3px;cursor:pointer;transition:ease;">Login</button>';
+    document.getElementById('login-btn').addEventListener('click', showLoginPopup);
+  }
+}
+
+// Function to check if the user is logged in
+function isLoggedIn() {
+  // Check if the authentication token exists in local storage
+  const token = localStorage.getItem('authToken');
+  return token !== null;
+}
+
+
+
+// Regular expression for password validation
+// This example requires a minimum of 8 characters, at least one uppercase letter, one lowercase letter, and one digit
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
 
 // Show login popup
 showPopupBtn.addEventListener("click", () => {
@@ -61,10 +91,15 @@ function showMovies(movies) {
         </div>
         `
         movieEl.addEventListener('click', () => {
-            // Log the clicked movie title to the console
-            console.log('Clicked Movie Title:', title);
-            // Open html.html in a new window or tab
-            window.location.href = `html.html?movieTitle=${encodeURIComponent(title)}`;
+            // Check if the user is logged in
+            if (isLoggedIn()) {
+                // Log the clicked movie title to the console
+                console.log('Clicked Movie Title:', title);
+                // Open html.html in a new window or tab
+                window.location.href = `html.html?movieTitle=${encodeURIComponent(title)}`;
+            } else {
+                alert('Please log in to view movie details.');
+            }
         });
         
         main.appendChild(movieEl)
@@ -94,3 +129,115 @@ form.addEventListener('submit', (e) => {
         window.location.reload()
     }
 })
+
+// Function to validate username
+function validateUsername(username) {
+  // Adjust the regular expression based on your username requirements
+  const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
+  return usernameRegex.test(username);
+}
+
+// Function to validate password
+function validatePassword(password) {
+  return passwordRegex.test(password);
+}
+
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const username = document.getElementById('login-username').value;
+  const password = document.getElementById('login-password').value;
+
+  if (!validateUsername(username)) {
+    alert('Please enter a valid username');
+    return;
+  }
+
+  if (!validatePassword(password)) {
+    alert('Please enter a valid password (minimum 8 characters, at least one uppercase letter, one lowercase letter, and one digit).');
+    return;
+  }
+
+  // Continue with login logic if validation passes
+  // ...
+  login(username, password);
+});
+
+const userData = [{ username: 'trigun', password: 'passWORD123' },];
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const username = document.getElementById('signup-username').value;
+  const password = document.getElementById('signup-password').value;
+
+  if (!validateUsername(username)) {
+    alert('Please enter a valid email address.');
+    return;
+  }
+
+  if (!validatePassword(password)) {
+    alert('Please enter a valid password (minimum 8 characters, at least one uppercase letter, one lowercase letter, and one digit).');
+    return;
+  }
+  
+  const existingUser = userData.find((user) => user.username === username);
+
+  if (existingUser) {
+    alert('Username is already taken. Please choose another.');
+    return;
+  }
+  // Continue with signup logic if validation passes
+  userData.push({ username, password });
+  alert(`User data for ${username} saved successfully!`);
+  const token = 'your_auth_token';
+
+  // Store the token in local storage
+  localStorage.setItem('authToken', token);
+
+  // Update the UI with the logged-in username
+  updateUserStatus(username);
+
+  // Display a welcome message
+  alert(`Welcome, ${username}!`);
+
+  closeSignupPopup();
+
+});
+
+const DUMMY_USERNAME = 'trigun';
+const DUMMY_PASSWORD = 'passWORD123';
+function login(username, password) {
+  const authenticatedUser = userData.find(
+    (user) => user.username === username && user.password === password);
+   if (authenticatedUser) {
+  const token = 'your_auth_token';
+
+  // Store the token in local storage
+  localStorage.setItem('authToken', token);
+
+  // Update the UI
+  updateUserStatus(username);
+
+
+  closeLoginPopup();
+} else {
+  // Display an alert for incorrect credentials
+  alert('Invalid username or password. Please try again.');
+}
+}
+
+// Function to log out the user
+function logout() {
+  // Remove the authentication token from local storage
+  localStorage.removeItem('authToken');
+
+  // Update the UI
+  updateUserStatus();
+}
+
+function showLoginPopup() {
+  document.body.classList.add("show-popup");
+}
+
+// Function to close the login popup
+function closeLoginPopup() {
+  document.body.classList.remove("show-popup");
+}
